0e0820482f4a998514703bce3d64c1dc1ccdf4e5
Chandrasen Reddy <chandrasen265@gmail.com>
2023-05-20 06:10:51 -0700

    few modifications to file structure

--- Threads/src/AppTest.java
+++
@@ -1,8 +0,0 @@
-import java.util.stream.Stream;
-
-public class AppTest {
-    public static void main(String[] args) {
-        Application application=new Application();
-        Assert.assertEquals(application.getData()).toEqual(null);
-    }
-}
