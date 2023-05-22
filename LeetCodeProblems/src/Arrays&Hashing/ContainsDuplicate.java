f5be0a6573f82b807e4e08db3029ddc88fc86bc9
Chandrasen Reddy <chandrasen265@gmail.com>
2023-05-22 15:17:03 -0700

    "Valid anagram solution using HashMap"

--- LeetCodeProblems/src/Arrays&Hashing/ContainsDuplicate.java
+++ LeetCodeProblems/src/Arrays&Hashing/ContainsDuplicate.java
@@ -1,20 +1,15 @@
 public class ContainsDuplicate {
-    public static void main(String args[]) {
-        int[] nums = {1,2,3,1};
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        MrgeSort ob = new MrgeSort();
+        ob.sort(nums, 0, nums.length - 1);
         boolean answer = false;
-        for (int i = 0; i < nums.length; i++) {
-            if (answer == true){
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i+1]){
+                answer = true;
                 break;
-            }
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    answer = true;
-                    break;
-                } else {
-                    answer = false;
-                }
             }
         }
         System.out.println(answer);
     }
-}
+    }
