7f5a522a8a94cdeda932e8d4385135c642bd2138
Chandrasen Reddy <chandrasen265@gmail.com>
2023-05-22 10:33:33 -0700

    Merge Sort

--- LeetCodeProblems/src/Arrays&Hashing/ContainsDuplicate.java
+++ LeetCodeProblems/src/Arrays&Hashing/ContainsDuplicate.java
@@ -1,5 +1,20 @@
 public class ContainsDuplicate {
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String args[]) {
+        int[] nums = {1,2,3,1};
+        boolean answer = false;
+        for (int i = 0; i < nums.length; i++) {
+            if (answer == true){
+                break;
+            }
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    answer = true;
+                    break;
+                } else {
+                    answer = false;
+                }
+            }
+        }
+        System.out.println(answer);
     }
 }
--- producerconsumer/src/Application.java
+++ producerconsumer/src/Application.java
@@ -11,3 +11,4 @@ public class Application {
         t2.start();
     }
 }
+
