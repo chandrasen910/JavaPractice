491514a2173379d5867b5587dbefdfc3ed9f4271
Chandrasen Reddy <chandrasen265@gmail.com>
2023-07-03 06:50:42 -0700

    217 problem solution

---
+++ LeetCodeProblems/July/217.py
@@ -0,0 +1,5 @@
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+nums = [ 1,2,3,1]
+a = set(nums)
+print(len(a)!= len(nums))
+
