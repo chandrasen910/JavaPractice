3da21ff8c1e45f05838953b57ec3e07f10ae1756
Chandrasen Reddy <chandrasen265@gmail.com>
2023-07-04 23:02:15 -0700

    242 solution

--- LeetCodeProblems/July/242.py
+++ LeetCodeProblems/July/242.py
@@ -1,1 +1,13 @@
 #An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dict1 = {}
+        dict2 = {}
+        for i in range(len(s)):
+                dict1[s[i]] = 1 + dict1.get(s[i], 0)
+        for i in range(len(t)):
+                dict2[t[i]] = 1 + dict2.get(t[i],0)
+        if (dict1 == dict2):
+            return True
+        else:
+            return False
