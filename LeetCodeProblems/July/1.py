5aaffc112a0fa88abb48953ef7e269e14de6644d
Chandrasen <chandrasen265@gmail.com>
2023-07-18 21:57:54 -0700

    - 1, 49 solutions

--- LeetCodeProblems/July/1.py
+++ LeetCodeProblems/July/1.py
@@ -5,3 +5,15 @@
 # Input: nums = [2,7,11,15], target = 9
 # Output: [0,1]
 # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+from ast import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {} # to store value and index of elapsed array
+        for i , n in  enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        
